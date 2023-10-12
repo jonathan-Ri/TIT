eaf40cc62adaa0fd279a316d59f42bf6df96bdfe
Jonathan Ríos <jonathan.rios@alumnos.uv.cl>
2023-10-12 20:42:00 -0300

    home listo

--- iadc/src/App.js
+++ iadc/src/App.js
@@ -1,39 +1,38 @@
 //imports
 
-import Home from './Components/Home.component'
+import Home from './Components/Home/home.component'
 import logo from './logo.svg';
 import './App.css';
-import './Css/header.css';
+
 import React, { Component } from "react"; 
 
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Import Bootstrap
 import { Navbar, Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import NavDropdown from 'react-bootstrap/NavDropdown'
 
 // Import from react-router-dom
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+//import { Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="header">
-        header
+        
       </header>
-      <body>
         <Container>
           <Row>
             <Col md={12}>
-              <Router>
-                <Routes>
-                  <Route exact path="/" component={Home}/>
-                </Routes>
-              </Router>
+            <Router>
+             <Routes>
+               <Route path="/" element={<Home />} />
+               <Route path="/1" component={Home}/>
+             </Routes>
+           </Router>
             </Col>
           </Row>
         </Container>
-      </body>
     </div>
   );
 }
